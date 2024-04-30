c99018a67e8c416615b0d225f05cb5ae3e2ff804
Yona Mahundi <yona101992@gmail.com>
2024-04-30 23:01:41 +0300

    created unit controller

---
+++ src/main/java/com/yhm/smt/controller/UnitController.java
@@ -0,0 +1,38 @@
+package com.yhm.smt.controller;
+
+
+import com.yhm.smt.entity.Product;
+import com.yhm.smt.entity.Unit;
+import com.yhm.smt.service.ProductService;
+import com.yhm.smt.service.UnitService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Validated
+@RequestMapping(path = {"/units"})
+@RequiredArgsConstructor
+public class UnitController {
+    private final UnitService unitService;
+
+    @GetMapping
+    public Page<Unit> getAllUnits(Pageable pageable) {
+        return unitService.findAll(pageable);
+    }
+
+    @PostMapping
+    public void save(@Valid @RequestBody Unit unit) {
+        unitService.save(unit);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Unit> update(@PathVariable int id, @RequestBody Unit unit) {
+        Unit u = unitService.update(unit, id);
+        return ResponseEntity.ok(u);
+    }
+}
