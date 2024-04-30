a6d69e6ba3ee7b86c42fccc94272d7c19b5e2075
Yona Mahundi <yona101992@gmail.com>
2024-04-30 15:01:47 +0300

    initial commit

---
+++ src/main/java/com/yhm/smt/SmtApplication.java
@@ -0,0 +1,13 @@
+package com.yhm.smt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmtApplication.class, args);
+	}
+
+}
