8098bb8912b9207b08b4215f7d1b32607abee36a
Yona Mahundi <yona101992@gmail.com>
2024-04-30 22:57:43 +0300

    created unit service

---
+++ src/main/java/com/yhm/smt/repository/UnitRepository.java
@@ -0,0 +1,7 @@
+package com.yhm.smt.repository;
+
+import com.yhm.smt.entity.Unit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UnitRepository extends JpaRepository<Unit,Integer> {
+}
