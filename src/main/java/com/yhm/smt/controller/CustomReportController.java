02b5e5cc9c96c557512257688b97f88bbc9df9a9
Yona Mahundi <yona101992@gmail.com>
2024-05-04 03:48:56 +0300

    create custom report controller

--- src/main/java/com/yhm/smt/controller/CustomReportController.java
+++ src/main/java/com/yhm/smt/controller/CustomReportController.java
@@ -1,14 +1,41 @@
 package com.yhm.smt.controller;
 
 
+import com.yhm.smt.entity.CustomReport;
+import com.yhm.smt.entity.Unit;
+import com.yhm.smt.service.CustomReportService;
+import com.yhm.smt.service.UnitService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Validated
 @RequestMapping(path = {"/api/custom-report"})
 @RequiredArgsConstructor
 public class CustomReportController {
+    private final CustomReportService customReportService;
+
+    @GetMapping("")
+    public List<CustomReport> getAllUnits() {
+        return customReportService.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomReport> save(@Valid @RequestBody CustomReport customReport) {
+
+        customReportService.save(customReport);
+        return ResponseEntity.ok(customReport);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<CustomReport> update(@PathVariable int id, @RequestBody CustomReport customReport) {
+        CustomReport c = customReportService.update(customReport, id);
+        return ResponseEntity.ok(c);
+    }
+
 }
